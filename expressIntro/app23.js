9e5d51eaa3bef95b2c29235b15a5076782fdbca9
aabellanosa <auabellanosa@hotmail.com>
2020-07-30 13:45:30 +0800

    adding express

--- expressIntro/app23.js
+++ expressIntro/app23.js
@@ -10,9 +10,5 @@ app.get("/contact", function (req, res) {
   res.send("this is the contact page");
 });
 
-app.get("/profile/:id", function (req, res) {
-  res.send("You requested to see the profile of id: " + req.params.id);
-});
-
 app.listen(3000);
 console.log("now listening on port 3000");
